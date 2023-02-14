b6f3650ea312b31d0f0ef6c69b29608255771a9d
Max Sprang <masprang@uni-mainz.de>
2023-02-14 16:18:18 +0100

    track plots

--- trackplots_genes.r
+++ trackplots_genes.r
@@ -7,14 +7,16 @@ library(TxDb.Hsapiens.UCSC.hg38.knownGene)
 library(openxlsx)
 library(ggplot2)
 library(clusterProfiler)
-library(org.Hs.eg.db)
 library(gridExtra)
-
 library(Gviz)
+library(tidyverse)
 
 # genes of special interest
-gene_list <- c("CMYC", "MYC",  "JUN", "YY1", "E2F3", "GSTO1", "NOP56", "NUDT17")
-
+gene_list <- c( "MYC",  "JUN", "YY1", "E2F3", "GSTO1", "NOP56", "NUDT17")
+gene_entrz <- AnnotationDbi::select(org.Hs.eg.db, 
+                                    keys = gene_list,
+                                    columns = "ENTREZID",
+                                    keytype="SYMBOL")
 
 txdb <- TxDb.Hsapiens.UCSC.hg38.knownGene
 
@@ -23,9 +25,38 @@ bam.folder <- "bams"
 
 gtrack <- GenomeAxisTrack()
 
-select(txdb, keys = gene_list, columns=)
+txdb_gene_interest <- AnnotationDbi::select(txdb, 
+                                            keys = gene_entrz$ENTREZID,
+                                            columns=columns(txdb),
+                                            keytype="GENEID")
+txdb_gene_interest <- txdb_gene_interest %>% as_tibble()
 
-txTr <- GeneRegionTrack(txdb, chromosome = "chr6", 
-                        start = 35000000,  end = 40000000)
+for (i in 1:length(gene_list)) {
+    symbol <- gene_list[i]
+    id <- gene_entrz[gene_entrz$SYMBOL == symbol,"ENTREZID"]
+
+    # get adress
+    # chr should be singular after unqiue, while multiple starts/ends remain
+    # use the min start and the max end to cover the complete gene
+    chr <- txdb_gene_interest %>%
+        dplyr::filter(GENEID == id) %>%
+        pull("EXONCHROM") %>%
+        unique() 
+    start <- txdb_gene_interest %>%
+        dplyr::filter(GENEID == id) %>%
+        pull("EXONSTART") %>%
+        unique()
+    end <- txdb_gene_interest %>%
+        dplyr::filter(GENEID == id) %>%
+        pull("EXONEND") %>%
+        unique()
+    txTr <- GeneRegionTrack(txdb,
+                            chromosome = chr, 
+                            start = min(start),
+                            end = max(end))
+    plotTracks(c(gtrack, txTr))
+}
+
+
 
 plotTracks(c(gtrack, txTr))
